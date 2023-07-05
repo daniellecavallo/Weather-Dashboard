d23ab22d1618b3a0ed468fde77e296e0f0648396
University <university@AA687692-Mac.local>
2023-07-05 16:21:58 -0500

    linked js file

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ var cityName = $("#cityname")
 var cityDate = $("#citydate")
 var cityTempature = $("#citytempature")
 var cityHumidity = $("#humidity")
-var cityWindspeed = $("#citywindspeed")
+var cityWindspeed = $("#windspeed")
 var cityinput = $("#cityinput")
 var searchbutton = $("#searchbutton")
 $(function() {
+    searchbutton.on("click", function(event) {
+        event.preventDefault()
+        console.log("click")
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityinput.val()}&limit=1&appid=${apiKey}`)
+    .then( function(response) {
+    return response.json()
+    
+    })
+    .then( function(data) {
+    console.log(data)
+    })
+    })
+    
+})
 
-})
-searchbutton.on("click", function() {
-    console.log("click")
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityinput.val()}&limit=1&appid=${apiKey}`)
-.then( function(response) {
-return response.json()
-
-})
-.then( function(data) {
-console.log(data)
-})
-})
