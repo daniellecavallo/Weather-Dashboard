11ec634bdd0e1b1c432daade46a1b0c09e8878c5
University <university@AA687692-Mac.local>
2023-07-05 16:12:23 -0500

    functions

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ var cityTempature = $("#citytempature")
 var cityHumidity = $("#humidity")
 var cityWindspeed = $("#citywindspeed")
 var cityinput = $("#cityinput")
-var searchbutton =$("#searchbutton")
+var searchbutton = $("#searchbutton")
+$(function() {
 
+})
 searchbutton.on("click", function() {
+    console.log("click")
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityinput.val()}&limit=1&appid=${apiKey}`)
+.then( function(response) {
+return response.json()
+
+})
+.then( function(data) {
+console.log(data)
+})
 })
