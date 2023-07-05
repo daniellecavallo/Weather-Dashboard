73e11c0827eee16436b583edbded9c2053b57973
University <university@AA687692-Mac.local>
2023-07-05 15:48:33 -0500

    added vars

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 var apiKey = "eed97305be64b7b9b5d3eb427b65c241"
+var cityName = $("#cityname")
+var cityDate = $("#citydate")
+var cityTempature = $("#citytempature")
+var cityHumidity = $("#humidity")
+var cityWindspeed = $("#citywindspeed")
+var cityinput = $("#cityinput")
