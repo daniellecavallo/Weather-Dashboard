de85811313fd1a5fb3bb6400118b6bf7bb03c85c
University <university@AA687692-Mac.local>
2023-07-07 15:55:34 -0500

    loops

--- script.js
+++ script.js
@@ -19,10 +19,24 @@ $(function() {
     console.log(data)
     var lat = data[0].lat
     var lon = data[0].lon
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`).then(function(response) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`).then(function(response) {
     return response.json()
     }).then(function(data) {
+    cityName.text(data.name) 
+    cityTempature.text(data.main.temp) 
+    cityHumidity.text(data.main.humidity) 
+    cityWindspeed.text(data.wind.speed) 
     })
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`).then(function(response) {
+        return response.json()
+        }).then(function(data) {
+        console.log(data)
+        for (let i = 0; i < data.list.length; i += 8) {
+        $(".forecast").each(function() {
+        
+        })
+        }
+        })
     })
     })
     
